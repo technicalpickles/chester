09dd79eb7d3327e6c4049498fe9c57bed9eb9fa5
Josh and James <josh+james@technicalpickles.com>
2009-07-18 16:28:14 -0400

    added what I think are the correct routes that cheat.errtheblog.com uses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :sheets
   map.root :controller => 'sheets'
+  
+  map.with_options :controller => 'sheets', :format => 'txt' do |sheets|
+    map.connect '/yr/', :action => 'recent'
+    map.connect '/ya/', :action => 'all'
+    map.connect '/y/:name', :action => 'show'
+  end
 end
