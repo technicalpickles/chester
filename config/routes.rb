ff65b1cc11c06494963f134ab3432f9906d34773
Josh and James <josh+james@technicalpickles.com>
2009-07-18 17:18:35 -0400

    Fixed legacy routes to be yaml.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :sheets
   map.root :controller => 'sheets'
   
-  #map.with_options :controller => 'sheets', :format => 'txt' do |sheets|
+  #map.with_options :controller => 'sheets', :format => 'yaml' do |sheets|
   #  map.connect '/yr/', :action => 'recent'
   #  map.connect '/ya/', :action => 'all'
   #  map.connect '/y/:name', :action => 'show'
